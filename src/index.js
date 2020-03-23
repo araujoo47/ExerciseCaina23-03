561710bd19d00a654d89c3ae08b77aafbf630bf7
araujo <tiago18.araujoo@gmail.com>
2020-03-23 12:44:53 +0000

    done

--- src/index.js
+++ src/index.js
@@ -1,36 +1,45 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import ReactDOM from "react-dom";
 import cx from "classnames";
-
 import "./styles.css";
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { count: 100 };
+    this.state = { count: 100, error: "" };
     this.liked = false;
   }
 
   like = () => {
-    if (!this.liked) {
-      this.liked = true;
-      this.setState(({ count }) => ({ count: count + 1 }));
-      return;
-    }
-    this.liked = false;
-    this.setState(({ count }) => ({ count: count - 1 }));
+    fetch("http://localhost:3000/")
+      .then(data => data.json())
+      .then(message => {
+        console.log(message);
+        if (message) {
+          this.setState({ error: "" });
+          if (!this.liked) {
+            this.liked = true;
+            this.setState(({ count }) => ({ count: count + 1 }));
+            return;
+          }
+          this.liked = false;
+          this.setState(({ count }) => ({ count: count - 1 }));
+        } else { 
+          this.setState({ error: "404" });
+        }
+      });
   };
 
   render() {
-    const { count } = this.state;
+    const { count, error } = this.state;
     return (
-      <button
-        type="button"
-        onClick={this.like}
-        className={cx("like-button", { liked: this.liked })}
-      >
-        Like | <span className="likes-counter">{count}</span>
-      </button>
+      <Fragment>
+        <button
+          type="button"
+          onClick={this.like}
+          className={cx("like-button", { liked: this.liked })} > Like | <span className="likes-counter">{count}</span> </button>
+        <p>{error}</p>
+      </Fragment>
     );
   }
 }
